4c2d82473508fe1b5371c274f5a60d2bfdac0788
Tissiana <laratissiana@gmail.com>
2019-05-13 17:29:26 -0700

    start working on design

--- mobile/App.js
+++ mobile/App.js
@@ -3,18 +3,7 @@ import { StyleSheet, Text, View } from 'react-native'
 
 import { AppLoading, Asset } from 'expo'
 import Navigation from './navigation2/index'
-/* import * as constants from './constants'
-import { Block } from './components' */
 
-/* const images = [
-  //we're importing all images
-
-  require('./assets/images/grady_1.jpg'),
-  require('./assets/images/grady_2.jpg'),
-  require('./assets/images/grady_3.jpg'),
-  require('./assets/images/grady_4.jpeg'),
-  require('./assets/images/grady_5.jpg'),
-]; */
 
 export default class App extends React.Component {
   state = {
--- mobile/screens2/Welcome.js
+++ mobile/screens2/Welcome.js
@@ -1,54 +1,64 @@
 import React, { Component } from 'react'
 import {
+    Image,
     StyleSheet,
-    Modal,
+    FlatList,
     ScrollView,
     View,
     Text,
-    Button
+    Animated
 } from 'react-native'
 import navigation from '../navigation2/index';
+import Background from '../assets/grady3.jpg'
+import { Button } from 'react-native-material-ui';
+
+// import { ScrollView } from 'react-native-gesture-handler';
+// import { Animated } from "react-animated-css";
+
+
 
 class Welcome extends Component {
+    // constructor(props) {
+    //     super(props);
+
+    //     this.state = {
+    //         showTerms: true
+    //     }
+    // }
     // static navigationOptions = {
     //     header: null
     // }
 
-    // state = {
-    //     showTerms: false
-    // }
-
     // renderTermsService() {
     //     return (
-    //         <Modal animationType="slide" visible={this.state.showTerms}>
-    //             <View space='between'>
-    //                 <Text h2 light>Terms of Service</Text>
+    //         <Animated animationIn="slideInUp" visible={this.state.showTerms}>
+    //             <View>
+    //                 <Text>Terms of Service</Text>
     //                 <ScrollView>
-    //                     <Text caption gray height={18}>
+    //                     <Text>
     //                         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     //                 </Text>
-    //                     <Text caption gray height={18}>
+    //                     <Text>
     //                         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     //                 </Text>
-    //                     <Text caption gray height={18}>
+    //                     <Text>
     //                         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     //                 </Text>
-    //                     <Text caption gray height={18}>
+    //                     <Text>
     //                         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     //                 </Text>
-    //                     <Text caption gray height={18}>
+    //                     <Text>
     //                         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     //                 </Text>
-    //                     <Text caption gray height={18}>
+    //                     <Text>
     //                         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     //                 </Text>
     //                 </ScrollView>
 
-    //                 <Button title="I understand" gradient onPress={() => this.setState({ showTerms: false })}>
-    //                     {/* <Text center white>I understand</Text> */}
+    //                 <Button title="I understand" onPress={() => this.setState({ showTerms: false })}>
     //                 </Button>
     //             </View>
-    //         </Modal >
+    //         </Animated >
     //     )
     // }
 
@@ -56,49 +66,58 @@ class Welcome extends Component {
         const { navigation } = this.props;
 
         return (
-            <View style={styles.container}>
-                <View center bottom flex={0.5}>
-                    <Text style={styles.welcome}>Don't Walk. Alone.</Text>
-                    <Text style={styles.welcome2}>We'll watch over you.</Text>
-                </View>
-                <View middle>
-                    <Button title="Login" gradient onPress={() => navigation.navigate('Login')}>
-                    </Button>
-                    <Button title="SignUp" shadow onPress={() => navigation.navigate('Signup')}>
-                    </Button>
-                    <Button title="Terms of Service" onPress={() => this.setState({ showTerms: true })} >
-                    </Button>
-                </View>
-                {/* {this.renderTermsService()} */}
-            </View >
-
+            <View>
+                <Image source={Background} style={{ width: '100%', position: 'absolute', zIndex: -1 }} />
+                <View style={styles.container}>
+                    <View center bottom flex={0.5}>
+                        <Text style={styles.welcome}>Don't Walk. Alone.</Text>
+                        <Text style={styles.welcome2}>We'll watch over you.</Text>
+                    </View>
+                    <View>
+                        {/* <Button title='Login' variant="contained" onPress={() => navigation.navigate('Login')}>
+                        </Button> */}
+                        <Button raised primary text="Login" title='Login' variant="contained" onPress={() => navigation.navigate('Login')} />
+                        {/* <Button title='Signup' variant="outlined" onPress={() => navigation.navigate('Signup')}>
+                        </Button> */}
+                        <Button accent primary text="Signup" title='Signup' variant="outlined" onPress={() => navigation.navigate('Signup')} />
+                        {/* <Button title='Terms of Service' onPress={() => this.setState({ showTerms: true })}>
+                        </Button> */}
+                        <Button primary text="Terms of Service" title='Terms of Service' onPress={() => this.setState({ showTerms: true })} />
+                    </View>
+                    {/* {this.renderTermsService()} */}
+                </View >
+            </View>
         )
     }
 }
 
-// Welcome.defaultProps = {
-// }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#E8E8E8',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        position: 'absolute',
+        marginTop: '100%',
+        marginLeft: '20%'
     },
     welcome:
     {
         color: '#000000',
         fontSize: 30,
-        marginBottom: 150,
 
     },
     welcome2:
     {
         color: '#B0C4DE',
         fontSize: 18,
-        marginTop: -120,
-        marginLeft: 30
+
+    },
+    backgroundImage: {
+        flex: 1,
+        width: null,
+        height: null,
+        resizeMode: 'cover'
     }
 
 })
