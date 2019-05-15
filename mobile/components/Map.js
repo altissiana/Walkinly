cec1ba8910268c818193e87992346c52d11d5923
Austin Marler <austinnmarler@gmail.com>
2019-05-14 17:08:55 -0700

    finished location data and some button functionality

--- mobile/components/Map.js
+++ mobile/components/Map.js
@@ -3,16 +3,45 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 
 export default class SimpleMap extends React.Component {
+  state = {
+    userLocation: {
+      latitude: 0,
+      longitude: 0,
+      latitudeDelta: 0.1,
+      longitudeDelta: 0.1
+    }
+  }
+
+  componentDidMount() {
+    this.getUserLocation()
+  }
+
+  getUserLocation = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.setState({
+        userLocation: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1
+        }
+      })
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <MapView
+          followsUserLocation={true}
+          showsScale={true}
+          showsUserLocation={true}
           style={styles.map}
           region={{
-            latitude: 59.329323349999999,
-            longitude: 18.068580800000063,
-            latitudeDelta: 0.1,
-            longitudeDelta: 0.1
+            latitude: this.state.userLocation.latitude,
+            longitude: this.state.userLocation.longitude,
+            latitudeDelta: this.state.userLocation.latitudeDelta,
+            longitudeDelta: this.state.userLocation.longitudeDelta
           }}
         />
       </View>
