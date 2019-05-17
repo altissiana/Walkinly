91aa79b93658d7cb02c0f4c83eae27586b51b0c2
Tissiana <laratissiana@gmail.com>
2019-05-17 16:23:17 -0700

    fixed merge

--- mobile/App.js
+++ mobile/App.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-
-import { AppLoading, Asset } from 'expo'
-import Navigation from './navigation'
-import * as constants from './constants'
-import { Block } from './components'
-
-const images = [
-  //we're importing all images
-
-  require('./assets/images/grady_1.jpg'),
-  require('./assets/images/grady_2.jpg'),
-  require('./assets/images/grady_3.jpg'),
-  require('./assets/images/grady_4.jpeg'),
-  require('./assets/images/grady_5.jpg'),
-];
-
-export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false
-  }
-
-  handleResourcesAsync = async () => {
-    //we're caching all the images for better performance  
-    const cacheImages = images.map(img => {
-      return Asset.fromModule(image).downloadAsync();
-    });
-
-    return Promise.all(cacheImages);
-  }
-=======
 import React from 'react';
 import { Platform, StatusBar, Text, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
@@ -41,26 +8,11 @@ export default class App extends React.Component {
   state = {
     isLoadingComplete: false,
   };
->>>>>>> 33b966e1ba5939a4357fca82ac6cb9581961d208
 
   render() {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
       return (
         <AppLoading
-<<<<<<< HEAD
-          startAsync={this.handleResourcesAsync}
-          onError={error => console.warn(error)}
-          onFinish={() => this.setState({ isLoadingComplete: true })}
-        />
-      )
-    }
-
-    return (
-      <Block white>
-        <Navigation />
-      </Block>
-    );
-=======
           startAsync={this._loadResourcesAsync}
           onError={this._handleLoadingError}
           onFinish={this._handleFinishLoading}
@@ -75,7 +27,6 @@ export default class App extends React.Component {
         </View>
       );
     }
->>>>>>> 33b966e1ba5939a4357fca82ac6cb9581961d208
   }
 
   _loadResourcesAsync = async () => {
@@ -106,8 +57,6 @@ export default class App extends React.Component {
 }
 
 const styles = StyleSheet.create({
-<<<<<<< HEAD
-=======
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -117,5 +66,4 @@ const styles = StyleSheet.create({
     height: 20,
     width: "100%"
   },
->>>>>>> 33b966e1ba5939a4357fca82ac6cb9581961d208
 });
