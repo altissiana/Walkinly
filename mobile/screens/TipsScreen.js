b08409270ecef6df27951b86a945ba163200955a
Austin Marler <austinnmarler@gmail.com>
2019-05-17 17:58:08 -0700

    minor changes

--- mobile/screens/TipsScreen.js
+++ mobile/screens/TipsScreen.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import { View, Text } from "react-native";
 
-export default class TipsScreen extends React.Component {
+export default class TipsScreen extends Component {
   static navigationOptions = {
     header: null
   }
