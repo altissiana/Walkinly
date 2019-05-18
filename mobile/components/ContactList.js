10ec46dc98c76ec3b42f58bccadad548ca7d7440
Raul Gonzalez <raulgonzalez1770@gmail.com>
2019-05-18 09:52:54 -0700

    backend

--- mobile/components/ContactList.js
+++ mobile/components/ContactList.js
@@ -2,13 +2,23 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class Contacts extends Component {
+  state = {
+    name: "",
+    phoneNumber: ""
+  };
+  componentDidMount() {
+    getContacts();
+  }
   render() {
     return (
       <div className="contactList">
         <h1> Emergency Contacts</h1>
         <nav>
           <ul>
-            <li>Name: Phone Number: Email:</li>
+            <li>
+              Name: {}
+              Phone Number:
+            </li>
           </ul>
         </nav>
       </div>
