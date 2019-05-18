5faa10d64b2a654fd8ed87a0f486f5444c8a9980
Tissiana <laratissiana@gmail.com>
2019-05-18 15:33:32 -0700

    created tips screen

--- mobile/screens/LoginScreen.js
+++ mobile/screens/LoginScreen.js
@@ -20,7 +20,7 @@ export default class Login extends Component {
         loading: false,
         isMounted: false
     }
-
+y
     componentDidMount () {
         this.setState({
             isMounted: true
