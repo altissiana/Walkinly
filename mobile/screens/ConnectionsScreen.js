5be7d0a37e13184ce982f20f42cf33eb32221e18
Raul Gonzalez <raulgonzalez1770@gmail.com>
2019-05-19 14:02:20 -0700

    added styles

--- mobile/screens/ConnectionsScreen.js
+++ mobile/screens/ConnectionsScreen.js
@@ -1,27 +1,23 @@
 import React, { Component } from "react";
-import { 
-  View, 
-  ScrollView, 
-  Text, 
-  AsyncStorage 
-} from "react-native";
+import { View, ScrollView, Text, AsyncStorage, StyleSheet } from "react-native";
 import { getConnections } from "../actions/Actions";
+import { brotliDecompress } from "zlib";
 
 export default class ConnectionsScreen extends Component {
   static navigationOptions = {
     header: null
-  }
+  };
 
   state = {
     connections: [],
-    email: '',
+    email: "",
     isMounted: false
-  }
+  };
 
   componentDidMount() {
     this.setState({
       isMounted: true
-    })
+    });
     this.getToken().then(() => {
       this.getConnections();
     });
@@ -29,50 +25,58 @@ export default class ConnectionsScreen extends Component {
 
   getToken = async () => {
     try {
-      const token = await AsyncStorage.getItem('userToken')
+      const token = await AsyncStorage.getItem("userToken");
       this.setState({
         email: token
-      })
+      });
     } catch (e) {
       console.log("getToken Error: " + e);
       throw new Error(e);
     }
-  }
+  };
 
   getConnections = async () => {
     if (this.state.isMounted) {
-      await getConnections(this.state.email).then((data) => {
+      await getConnections(this.state.email).then(data => {
         this.setState({
           connections: data
-        })
-      })
+        });
+      });
     }
-  }
+  };
 
   componentWillUnmount() {
     this.setState({
       isMounted: false
-    })
+    });
   }
 
   render() {
     return (
       <ScrollView
-        contentContainerStyle={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        contentContainerStyle={{
+          flex: 1,
+          flexDirection: "column",
+          justifyContent: "center",
+          fontSize: 24
+        }}
       >
         <Text>Connections</Text>
-        {this.state.connections 
-          ? this.state.connections.map((contact, i) => {
-              return (<View key={'contact' + i}>
+        {this.state.connections ? (
+          this.state.connections.map((contact, i) => {
+            return (
+              <View key={"contact" + i}>
                 <Text>
                   Name:{contact.FirstName} {contact.LastName}
                 </Text>
                 <Text>Phone Number: {contact.PhoneNumber}</Text>
-              </View>)
-            })
-          : <View></View>
-        }
+              </View>
+            );
+          })
+        ) : (
+          <View />
+        )}
       </ScrollView>
-    )
+    );
   }
 }
