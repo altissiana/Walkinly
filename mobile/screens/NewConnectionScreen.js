0c394534999f876df6dc8fdd6442b229ff872f8a
Raul Gonzalez <raulgonzalez1770@gmail.com>
2019-05-25 14:02:55 -0700

    added saving new connection to db and styling

--- mobile/screens/NewConnectionScreen.js
+++ mobile/screens/NewConnectionScreen.js
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
-import { TextInput, View, Button, StyleSheet, Text, Alert } from "react-native";
+import {
+  TextInput,
+  View,
+  Button,
+  StyleSheet,
+  Text,
+  Alert,
+  AsyncStorage
+} from "react-native";
+import { newConnection } from "../actions/Actions";
+
 class NewConnectionScreen extends Component {
   static navigationOptions = ({ navigation }) => ({
     headerStyle: {
@@ -25,13 +35,17 @@ class NewConnectionScreen extends Component {
     PhoneNumber: ""
   };
 
-  saveConnection() {
-    this.setState({
-      FirstName: this.state.FirstName,
-      LastName: this.state.LastName,
-      PhoneNumber: this.state.PhoneNumber
+  saveConnection = async () => {
+    await AsyncStorage.getItem("userToken").then(email => {
+      console.log(this.state);
+      newConnection(
+        email,
+        this.state.PhoneNumber,
+        this.state.FirstName,
+        this.state.LastName
+      );
     });
-  }
+  };
 
   render() {
     return (
@@ -40,24 +54,39 @@ class NewConnectionScreen extends Component {
           style={styles.text}
           title="FirstName"
           placeholder="First Name"
+          defaultValue={this.state.FirstName}
+          onChangeText={text => {
+            this.setState({ FirstName: text });
+          }}
         />
         <TextInput
           style={styles.text}
           title="LastName"
           placeholder="Last Name"
+          defaultValue={this.state.LastName}
+          onChangeText={text => {
+            this.setState({ LastName: text });
+          }}
         />
         <TextInput
           style={styles.text}
           title="PhoneNumber"
           placeholder="Phone Number"
-        />
-        <Button
-          title="Fuckin Save"
-          style={styles.saveButton}
-          onPress={() => {
-            this.saveConnection();
+          defaultValue={this.state.PhoneNumber}
+          onChangeText={text => {
+            this.setState({ PhoneNumber: text });
           }}
         />
+        <View style={[{ width: "50%", marginLeft: 95 }]}>
+          <Button
+            title="Save"
+            style={styles.saveButton}
+            onPress={() => {
+              this.saveConnection();
+              this.props.navigation.navigate("AuthLoading");
+            }}
+          />
+        </View>
       </View>
     );
   }
@@ -75,7 +104,7 @@ const styles = StyleSheet.create({
     paddingTop: 50
   },
   saveButton: {
-    width: 100
+    width: 40
   }
 });
 
