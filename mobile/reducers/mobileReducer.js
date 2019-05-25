91d421bc981c0119ea8ae2aa0c15c483a7deedbc
Tissiana <laratissiana@gmail.com>
2019-05-25 13:06:35 -0700

    added camera and image library to settings

--- mobile/reducers/mobileReducer.js
+++ mobile/reducers/mobileReducer.js
@@ -5,15 +5,17 @@ const initialState = {
 }
 
 export default function (state = initialState, action) {
-  switch(action.type) {
+  switch (action.type) {
     case "GET_CONNECTIONS":
-      return { ...state, 
-        connections: action.payload 
+      return {
+        ...state,
+        connections: action.payload
       }
     case "LOGIN/REGISTER/LOGOUT":
-      return { ...state, 
-        user: action.payload.user, 
-        isAuthenticated: action.payload.isAuthenticated 
+      return {
+        ...state,
+        user: action.payload.user,
+        isAuthenticated: action.payload.isAuthenticated
       }
     default:
       return state
