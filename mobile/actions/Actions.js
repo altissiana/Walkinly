f30d2f95c7a53d23e6aabaaa7a738d586c1d1f7b
Raul Gonzalez <raulgonzalez1770@gmail.com>
2019-05-28 12:58:09 -0700

    added some styling

--- mobile/actions/Actions.js
+++ mobile/actions/Actions.js
@@ -1,4 +1,3 @@
-import { ImagePicker, Permissions } from 'expo';
 import { AsyncStorage } from "react-native";
 import store from "../store";
 import axios from "axios";
@@ -6,19 +5,19 @@ import axios from "axios";
 export function signout() {
   return new Promise((resolve, reject) => {
     AsyncStorage.removeItem("userToken");
-    AsyncStorage.removeItem('userPic')
+    AsyncStorage.removeItem("userPic");
     store.dispatch({
       type: "LOGIN/REGISTER/LOGOUT",
       payload: {
         user: null,
         isAuthenticated: false
       }
-    })
+    });
     resolve();
   }).catch(err => {
     const error = err.response.data.error;
     reject(error);
-  })
+  });
 }
 
 export function getConnections(email) {
@@ -29,83 +28,92 @@ export function getConnections(email) {
         store.dispatch({
           type: "GET_CONNECTIONS",
           payload: resp.data
-        }) 
+        });
         resolve();
       })
       .catch(err => {
         reject(err);
-      })
-  })
+      });
+  });
 }
 
 export function signin(email, password) {
   return new Promise((resolve, reject) => {
     axios
       .post("http://10.68.0.155:3001/api/login", { email, password })
-      .then(async (resp) => {
+      .then(async resp => {
         const { email, name, phonenumber } = resp.data;
         await getConnections(email);
-        await AsyncStorage.setItem('userToken', email);
-        await AsyncStorage.setItem('userName', name);
-        await AsyncStorage.setItem('userPhone', phonenumber);
+        await AsyncStorage.setItem("userToken", email);
+        await AsyncStorage.setItem("userName", name);
+        await AsyncStorage.setItem("userPhone", phonenumber);
         store.dispatch({
           type: "LOGIN/REGISTER/LOGOUT",
           payload: {
             user: email,
             isAuthenticated: true
           }
-        })
-        resolve()
+        });
+        resolve();
       })
       .catch(err => {
-        reject(err)
-      })
-  })
+        reject(err);
+      });
+  });
 }
 
 export function register(email, password, phonenumber, firstname, lastname) {
   return new Promise((resolve, reject) => {
     axios
-      .post("http://10.68.0.155:3001/api/register", { email, password, phonenumber, firstname, lastname })
-      .then(async (resp) => {
+      .post("http://10.68.0.155:3001/api/register", {
+        email,
+        password,
+        phonenumber,
+        firstname,
+        lastname
+      })
+      .then(async resp => {
         const { email, name, phonenumber } = resp.data;
-        await getConnections(email)
-        await AsyncStorage.setItem('userToken', email)
-        await AsyncStorage.setItem('userName', name)
-        await AsyncStorage.setItem('userPhone', phonenumber)
+        await getConnections(email);
+        await AsyncStorage.setItem("userToken", email);
+        await AsyncStorage.setItem("userName", name);
+        await AsyncStorage.setItem("userPhone", phonenumber);
         store.dispatch({
           type: "LOGIN/REGISTER/LOGOUT",
           payload: {
             user: email,
             isAuthenticated: true
           }
-        })
-        resolve()
+        });
+        resolve();
       })
       .catch(err => {
-        reject(err)
-      })
-  })
+        reject(err);
+      });
+  });
 }
 
 export function changePassword(email, newPassword) {
   return new Promise((resolve, reject) => {
     axios
-      .patch('http://10.68.0.155:3001/api/changePassword', { email, newPassword })
+      .patch("http://10.68.0.155:3001/api/changePassword", {
+        email,
+        newPassword
+      })
       .then(resp => {
-        resolve()
+        resolve();
       })
       .catch(err => {
         reject(err);
-      })
-  })
+      });
+  });
 }
 
 export function setSosLocation(sosLocation) {
   store.dispatch({
     type: "SET_SOS_LOCATION",
     payload: sosLocation
-  })
+  });
 }
 
 export function setSosStatus(isActive) {
@@ -137,7 +145,7 @@ export function newConnection(userEmail, phonenumber, firstname, lastname) {
 export function editConnection(userEmail, phonenumber, firstname, lastname) {
   return new Promise((resolve, reject) => {
     axios
-      .patch('http://10.68.0.155:3001/api/editConnection', {
+      .patch("http://10.68.0.155:3001/api/editConnection", {
         userEmail,
         phonenumber,
         firstname,
@@ -148,23 +156,26 @@ export function editConnection(userEmail, phonenumber, firstname, lastname) {
         resolve();
       })
       .catch(err => {
-        reject(err)
-      })
-  })
+        reject(err);
+      });
+  });
 }
 
 export function deleteConnection(userEmail, phonenumber) {
   return new Promise((resolve, reject) => {
     axios
-      .delete("http://10.68.0.155:3001/api/deleteConnection", { data: {
-        userEmail, phonenumber
-      }})
+      .delete("http://10.68.0.155:3001/api/deleteConnection", {
+        data: {
+          userEmail,
+          phonenumber
+        }
+      })
       .then(async resp => {
-        await getConnections(userEmail)
-        resolve()
+        await getConnections(userEmail);
+        resolve();
       })
       .catch(err => {
-        reject(err)
-      })
-  })
+        reject(err);
+      });
+  });
 }
--- mobile/screens/ConnectionsScreen.js
+++ mobile/screens/ConnectionsScreen.js
@@ -15,20 +15,24 @@ import { getConnections, deleteConnection } from "../actions/Actions";
 
 const isCloseToBottom = ({ layoutMeasurement, contentOffset, contentSize }) => {
   const paddingToBottom = 20;
-  return layoutMeasurement.height + contentOffset.y >=
-    contentSize.height - paddingToBottom;
+  return (
+    layoutMeasurement.height + contentOffset.y >=
+    contentSize.height - paddingToBottom
+  );
 };
 
 class ConnectionsScreen extends Component {
   static navigationOptions = {
     headerStyle: {
       height: 40,
-      backgroundColor: 'dodgerblue'
+      backgroundColor: "black",
+      borderColor: "gray",
+      borderTopWidth: 2
     },
     headerTitle: "Connections",
     headerTitleStyle: {
       fontSize: 32,
-      color: 'white'
+      color: "white"
     },
     headerTitleContainerStyle: {
       top: -16
@@ -56,7 +60,7 @@ class ConnectionsScreen extends Component {
       try {
         getConnections(await AsyncStorage.getItem("userToken"));
       } catch (e) {
-        console.log(e)
+        console.log(e);
       }
     }
   };
@@ -65,34 +69,33 @@ class ConnectionsScreen extends Component {
     this.props.navigation.navigate("NewConnection");
   }
 
-  deleteConnection = async (phonenumber) => {
+  deleteConnection = async phonenumber => {
     if (this.state.isMounted) {
-      await AsyncStorage.getItem('userToken').then(email => {
-        deleteConnection(email, phonenumber)
-          .then(() => {
-            this.props.navigation.navigate("AuthLoading");
-          })
-      })
+      await AsyncStorage.getItem("userToken").then(email => {
+        deleteConnection(email, phonenumber).then(() => {
+          this.props.navigation.navigate("AuthLoading");
+        });
+      });
     }
-  }
+  };
 
   editConnection = async (phonenumber, firstname, lastname) => {
     if (this.state.isMounted) {
-      await AsyncStorage.setItem('tempConnPhone', phonenumber)
+      await AsyncStorage.setItem("tempConnPhone", phonenumber)
         .then(async () => {
-          await AsyncStorage.setItem('tempConnFirst', firstname)
+          await AsyncStorage.setItem("tempConnFirst", firstname);
         })
         .then(async () => {
-          await AsyncStorage.setItem('tempConnLast', lastname)
+          await AsyncStorage.setItem("tempConnLast", lastname);
         })
         .then(() => {
-          this.props.navigation.navigate("EditConnection")
+          this.props.navigation.navigate("EditConnection");
         })
         .catch(error => {
-          console.log(error)
-        })
+          console.log(error);
+        });
     }
-  }
+  };
 
   componentWillUnmount() {
     this.setState({
@@ -109,7 +112,7 @@ class ConnectionsScreen extends Component {
           if (isCloseToBottom(nativeEvent)) {
             this.setState({
               accepted: true
-            })
+            });
           }
         }}
         scrollEventThrottle={32}
@@ -120,62 +123,63 @@ class ConnectionsScreen extends Component {
             this.addConnection();
           }}
         />
-        {this.props.connections 
-          && this.props.connections.map((contact, i) => {
-              return (
-                <TouchableOpacity 
-                  key={'contact' + i}
-                  style={styles.connection}
-                  onPress={() => {
-                    Alert.alert(
-                      `${contact.FirstName} ${contact.LastName}`,
-                      'Would you like to edit or call this connection?',
-                      [
-                        {
-                          text: 'Call',
-                          onPress: () => {
-                            Linking.openURL(`tel:${contact.PhoneNumber}`)
-                          }
-                        },
-                        {
-                          text: 'Edit', 
-                          onPress: () => {
-                            this.editConnection(contact.PhoneNumber, contact.FirstName, contact.LastName);
-                          }
-                        },
-                        {
-                          text: 'Delete',
-                          onPress: () => {
-                            this.deleteConnection(contact.PhoneNumber);
-                          }
-                        },
-                        {
-                          text: 'Cancel',
-                          onPress: () => console.log('Cancelled'),
-                          style: 'cancel',
-                        },
-                      ],
-                      {cancelable: false}
-                    )
-                  }}
-                >
+        {this.props.connections &&
+          this.props.connections.map((contact, i) => {
+            return (
+              <TouchableOpacity
+                key={"contact" + i}
+                style={styles.connection}
+                onPress={() => {
+                  Alert.alert(
+                    `${contact.FirstName} ${contact.LastName}`,
+                    "Would you like to edit or call this connection?",
+                    [
+                      {
+                        text: "Call",
+                        onPress: () => {
+                          Linking.openURL(`tel:${contact.PhoneNumber}`);
+                        }
+                      },
+                      {
+                        text: "Edit",
+                        onPress: () => {
+                          this.editConnection(
+                            contact.PhoneNumber,
+                            contact.FirstName,
+                            contact.LastName
+                          );
+                        }
+                      },
+                      {
+                        text: "Delete",
+                        onPress: () => {
+                          this.deleteConnection(contact.PhoneNumber);
+                        }
+                      },
+                      {
+                        text: "Cancel",
+                        onPress: () => console.log("Cancelled"),
+                        style: "cancel"
+                      }
+                    ],
+                    { cancelable: false }
+                  );
+                }}
+              >
+                <View>
+                  <Text>
+                    <Text style={styles.bold}>Name:</Text> {contact.FirstName}{" "}
+                    {contact.LastName}
+                  </Text>
 
-                  <View>
-
-                    <Text>
-                      <Text style={styles.bold}>Name:</Text> {contact.FirstName} {contact.LastName}
-                    </Text>
-
-                    <Text>
-                      <Text style={styles.bold}>Phone Number:</Text> {contact.PhoneNumber}
-                    </Text>
-
-                  </View>
-
-                </TouchableOpacity>
+                  <Text>
+                    <Text style={styles.bold}>Phone Number:</Text>{" "}
+                    {contact.PhoneNumber}
+                  </Text>
+                </View>
+              </TouchableOpacity>
             );
-          })
-        }
+          })}
       </ScrollView>
     );
   }
@@ -183,33 +187,33 @@ class ConnectionsScreen extends Component {
 
 const styles = StyleSheet.create({
   alignAndJustCenter: {
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center"
   },
   connection: {
-    width: '90%',
+    width: "90%",
     borderWidth: 1,
-    borderStyle: 'solid',
-    borderColor: 'rgb(210, 210, 210)',
+    borderStyle: "solid",
+    borderColor: "rgb(210, 210, 210)",
     padding: 10,
     borderRadius: 10,
     marginVertical: 10
   },
   bold: {
-    fontWeight: 'bold'
+    fontWeight: "bold"
   },
-  container:{
+  container: {
     flex: 1,
     fontSize: 24
   },
   deleteConnection: {
     fontSize: 20,
     padding: 14,
-    color: 'red',
-    position: 'absolute',
+    color: "red",
+    position: "absolute",
     right: 0
   }
-})
+});
 
 function mapStateToProps(appState, ownProps) {
   return {
