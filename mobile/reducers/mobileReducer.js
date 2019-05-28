a93ff3d2a80d02c6b8b074348188eaff9dd659bc
Austin Marler <austinnmarler@gmail.com>
2019-05-28 09:41:30 -0700

    bug fixing

--- mobile/reducers/mobileReducer.js
+++ mobile/reducers/mobileReducer.js
@@ -39,38 +39,6 @@ export default function(state = initialState, action) {
         ...state,
         sosLocation: action.payload
       };
-    case "GET_MARKERS":
-      let vehicleMarker = {};
-      let sosMarker = {};
-      if (action.payload[0].type == "vehicle") {
-        vehicleMarker = action.payload[0];
-      } else {
-        sosMarker = action.payload[0];
-      }
-      if (action.payload[1].type == "sos") {
-        sosMarker = action.payload[1];
-      } else {
-        vehicleMarker = action.payload[1];
-      }
-      return {
-        ...state,
-        sosLocation: {
-          coordinates: {
-            latitude: sosMarker.latitude,
-            longitude: sosMarker.longitude
-          },
-          title: sosMarker.title,
-          description: sosMarker.description
-        },
-        vehicleLocation: {
-          coordinates: {
-            latitude: vehicleMarker.latitude,
-            longitude: vehicleMarker.longitude
-          },
-          title: vehicleMarker.title,
-          description: vehicleMarker.description
-        }
-      };
     case "SET_SOS_STATE":
       return {
         ...state,
